7ac53570631bf9faed49306ec9793dc769a96647
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-05-03 11:31:37 -0700

    #include<stdio.h> int main() { 	char str[1000]; 	scanf("%[^ ]s",str); 	int i,sum=0; 	for(i=0;str[i]!=NULL;i++) 	{ 		int x=str[i]; 		if(x>='0' && x<='9') 		{ 			x=x-48; 			sum=sum+x; 		} 	} 	printf("%d",sum); }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char str[1000];
+	scanf("%[^
+]s",str);
+	int i,sum=0;
+	for(i=0;str[i]!=NULL;i++)
+	{
+		int x=str[i];
+		if(x>='0' && x<='9')
+		{
+			x=x-48;
+			sum=sum+x;
+		}
+	}
+	printf("%d",sum);
+}
