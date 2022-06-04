de8e8b7033471cc3c830c255008488c487095bd1
Pothulakalyan <96769120+Pothulakalyan@users.noreply.github.com>
2022-06-04 15:17:33 +0530

    #include<stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=1;i<n-1;i++)     {         if(ar[i]%2!=0)         {             if(ar[i-1]%2==0 && ar[i+1]%2==0)             {                 count++;             }         }     }     printf("%d",count); }

---
+++ count_odd_numbers_between_even_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=1;i<n-1;i++)
+    {
+        if(ar[i]%2!=0)
+        {
+            if(ar[i-1]%2==0 && ar[i+1]%2==0)
+            {
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
